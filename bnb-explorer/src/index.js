c36c5ce8fda663417ad0ee9686a48662c8e3bb63
Mwambui123 <maurinawambuia@gmail.com>
2023-11-24 16:06:30 +0300

    updated the index.js

--- bnb-explorer/src/index.js
+++ bnb-explorer/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Components/App';
+import WrapperComponent from './Components/WrapperComponent';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import WrapperComponent from './Components/WrapperComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <WrapperComponent />
   </BrowserRouter>
 );
 
