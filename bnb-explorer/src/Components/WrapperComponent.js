4ba74de03bd3abb74dc17ea361ec92f9ccbc6ed8
Mwambui123 <maurinawambuia@gmail.com>
2023-11-24 13:54:53 +0300

    created wrapper component and added style to it

---
+++ bnb-explorer/src/Components/WrapperComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import Services from './Services';
+import App from '../App'; 
+
+const WrapperComponent = () => {
+  return (
+    <div style={{ backgroundColor: 'silver', padding: '20px' }}>
+      <App/>
+    </div>
+  );
+};
+
+export default WrapperComponent;
