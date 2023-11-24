cb2e2c259e50af01ed1684046480b7128fe87758
Mwambui123 <maurinawambuia@gmail.com>
2023-11-24 17:11:57 +0300

    revised changes

--- bnb-explorer/src/Components/Services.js
+++ bnb-explorer/src/Components/Services.js
@@ -1,12 +1,10 @@
-import React, { useState, useEffect , } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import BookButton from './BookButton';
 
-
-const Services= () => {
+const Services = () => {
   const [data, setData] = useState([]);
   
-
   useEffect(() => {
     fetchData();
   }, []);
@@ -24,27 +22,42 @@ const Services= () => {
         console.error('Error fetching data:', error);
       });
   };
-  
 
 
   return (
-    <div>
-      <h1>Data from JSON Server</h1>
-      <SearchBar data={data}  />
+    <div style={{ fontFamily: 'Arial, sans-serif', maxWidth: '800px', margin: '0 auto' }}>
+      <h1 style={{ textAlign: 'center' }}>Data from JSON Server</h1>
+      <SearchBar data={data} />
 
-      <ul>
+      <ul style={{ listStyle: 'none', padding: 0 }}>
         {data.map(item => (
-          <li key={item.id}>
+          <li key={item.id} style={{ marginBottom: '20px', borderBottom: '1px solid #ccc', paddingBottom: '20px' }}>
             <strong>Name:</strong> {item.name} <br />
-            {item.image && <img src={item.image} alt={item.name} style={{ maxWidth: '500', maxHeight: '500px' }} />}<br/>
-            <br></br>
-           <strong>Bedrooms:</strong> {item.Bedroom} <br />
+            {item.image && (
+              
+              <img
+                src={item.image}
+                alt={item.name}
+                style={{ 
+                  maxHeight:"800px",
+                  maxWidth:"800PX",
+                  marginTop: '10px',
+                  border: '1px solid #ddd', 
+              
+          
+              }}
+                
+              />
+            
+            )}
+                        <br />
+            <strong>Bedroom:</strong> {item.Bedroom} <br />
+            <strong>Bathrooms:</strong> {item.Bathrooms} <br />
             <strong>Address:</strong> {item.Adress} <br />
             <strong>Status:</strong> {item.Status} <br />
-            <strong>Bathrooms:</strong> {item.Bathrooms} <br />
             <BookButton />
+            
           </li>
-          
         ))}
       </ul>
     </div>
