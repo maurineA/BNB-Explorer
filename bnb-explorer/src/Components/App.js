9403d4cfd5b5bdc417e4b8e90085159cd22b3db8
Mwambui123 <maurinawambuia@gmail.com>
2023-11-24 16:35:58 +0300

    added css to the form

--- bnb-explorer/src/Components/App.js
+++ bnb-explorer/src/Components/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import NavBar from './NavBar'
+import { Route ,Routes } from 'react-router-dom'
+import Home from './Home'
+import Services from './Services'
+import BookingForm from './BookingForm'
+import PostForm from './PostForm'
+import Reviews from './Reviews'
+import Footer from './Footer'
 
 function App() {
   return (
     <div>
-      
+      < NavBar />
+      <Routes>
+        <Route path='/'element={<Home/>}></Route>
+      <Route path="/services"element={<Services/>}  ></Route>
+      <Route path='/home'element={<Home/>}></Route>
+      <Route path= "/boking form"element={<BookingForm/>}></Route>
+      <Route path="/postForm"element={<PostForm/>}></Route>
+      <Route path="/reviews" element={<Reviews/>}></Route>
+          </Routes>
+          <Footer />
     </div>
   )
 }
--- bnb-explorer/src/index.js
+++ bnb-explorer/src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import WrapperComponent from './Components/WrapperComponent';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import WrapperComponent from './Components/WrapperComponent';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <StrictMode>
   <BrowserRouter>
     <WrapperComponent />
   </BrowserRouter>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
